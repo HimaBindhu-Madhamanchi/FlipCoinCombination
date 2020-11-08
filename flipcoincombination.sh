9d7a1ac2634282b728c6f034f609957541372fa5
HimaBindhu-Madhamanchi <73021730+HimaBindhu-Madhamanchi@users.noreply.github.com>
2020-11-08 19:29:17 +0530

    Update flipcoincombination.sh

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -8,26 +8,27 @@ coin[headheadtail]=0;
 coin[headtailtail]=0;
 count=0;
 
-addtoDictionary(){
-if [ $1 -eq 0 ]
-then
-coin[headheadhead]=`expr ${coin[headheadhead]} + 1`;
-elif [ $1 -eq 1 ]
-then
-coin[tailtailtail]=`expr ${coin[tailtailtail]} + 1`;
-elif [ $1 -eq 2 ]
-then
-coin[headheadtail]=`expr ${coin[headheadtail]} + 1`;
-else
-coin[headtailtail]=`expr ${coin[headtailtail]} + 1`;
-fi
+addtoDictionary()
+{
+  if [ $1 -eq 0 ]
+  then
+     coin[headheadhead]=`expr ${coin[headheadhead]} + 1`;
+  elif [ $1 -eq 1 ]
+  then
+     coin[tailtailtail]=`expr ${coin[tailtailtail]} + 1`;
+  elif [ $1 -eq 2 ]
+  then
+     coin[headheadtail]=`expr ${coin[headheadtail]} + 1`;
+  else
+    coin[headtailtail]=`expr ${coin[headtailtail]} + 1`;
+  fi
 }
 
 while [ $count -le 25 ]
 do
-flip=$((RANDOM%4));
-addtoDictionary $flip;
-((++count))
+  flip=$((RANDOM%4));
+  addtoDictionary $flip;
+  ((++count))
 done
 
 perheadheadhead=`expr ${coin[headheadhead]} \* 100`;
